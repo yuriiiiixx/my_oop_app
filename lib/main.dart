61ed6c5ed3fa8438d69dd7dffe70bc5c4a3d9c28
Jhon <jhonalexisastilla9@gmail.com>
2025-07-31 10:18:09 +0800

    feat(main.dart): add Hello, OOP! centered text

--- lib/main.dart
+++ lib/main.dart
@@ -89,7 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
+        child: Text(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -103,16 +103,8 @@ class _MyHomePageState extends State<MyHomePage> {
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          'Hello, OOP!',
+          style: TextStyle(fontSize: 24),
         ),
       ),
       floatingActionButton: FloatingActionButton(
